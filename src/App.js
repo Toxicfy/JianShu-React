409de716df7db21cc0b5d9ca2a02aeff80659156
Toxicfy <15927630261@163.com>
2018-09-25 00:09:08 +0800

    Add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 // 根组件
 import React, { Component } from "react";
+import { Route, BrowserRouter} from "react-router-dom";
 import Header from "./common/header";
 import { Provider } from "react-redux";
 import store from "./store";
+
+import Home from "./page/Home";
+import Detail from "./page/Detail";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+        <div>
+          <Header />
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact component={Home} />
+              <Route path="/detail" exact component={Detail} />
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     );
   }
